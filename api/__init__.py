228d436e3be02d5fb4a4c318c21c2efc8fea5c3d
basile <basile-lequeux@hotmail.fr>
2022-01-13 16:59:51 +0100

    add flask migration

--- api/__init__.py
+++ api/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from credentials import postgresql_dev
+from flask_migrate import Migrate
+
 app = Flask(__name__)
 CORS(app)
 
@@ -9,8 +11,9 @@ app.config[
     "SQLALCHEMY_DATABASE_URI"] = postgresql_dev
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route('/')
 def hello():
-    return 'got to /graphql route to send request'
+    return 'go to /graphql route to send request'
