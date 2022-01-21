f88df2efb9a271df165f73a67a5e49048ea680d5
basile <basile-lequeux@hotmail.fr>
2022-01-21 15:20:01 +0100

    update prod config

--- api/__init__.py
+++ api/__init__.py
@@ -7,12 +7,16 @@ import os
 app = Flask(__name__)
 CORS(app)
 config = dotenv_values()
+
 #development
 #app.config["SQLALCHEMY_DATABASE_URI"] = config["DATABASE_URL"]
+#app.config["SECRET_KEY"] = config["SECRET_KEY"]
 
 #Prod
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
