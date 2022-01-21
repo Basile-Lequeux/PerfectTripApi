115dc9e43960839957323dc385f103478a221029
basile <basile-lequeux@hotmail.fr>
2022-01-21 14:54:55 +0100

    get token

--- api/queries/get_post.py
+++ api/queries/get_post.py
@@ -5,6 +5,8 @@ from ariadne import convert_kwargs_to_snake_case
 @convert_kwargs_to_snake_case
 def getPost_resolver(obj, info, id):
     try:
+        request = info.context
+        token = request.headers.get("authorization")
         post = Post.query.get(id)
         payload = {
             "success": True,
