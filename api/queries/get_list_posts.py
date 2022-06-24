866b8bf8660ab20015c8aca18b23b6d095ad6967
basile <basile-lequeux@hotmail.fr>
2022-06-24 12:46:49 +0200

    FIX sql query get post by tags

--- api/queries/get_list_posts.py
+++ api/queries/get_list_posts.py
@@ -11,17 +11,19 @@ def list_posts_resolver(obj, info, current_user, token, tags):
             tags_string = ','.join(map(str, tags))
             with engine.connect() as db_connect:
                 query_string = """
-                        with t3 as (SELECT t1.tag_id, COUNT(*) cnt FROM public.tags t1
-                        WHERE T1.tag_id in ({}) group by 1 )
-                        SELECT t1.*, t2.* FROM public.tags t1, public.post t2, t3
-                        WHERE  t2.id = t1.post_id and t3.tag_id = t1.tag_id
-                        order by t3.cnt desc
-                        """.format(tags_string)
+                with t3 as (SELECT t1.post_id, COUNT(*) cnt FROM public.tags t1 WHERE T1.tag_id in ({}) 
+                 group by 1 )
+                SELECT  t2.* FROM public.post t2, t3
+                WHERE   t3.post_id = t2.id
+                order by t3.cnt desc;
+                """.format(tags_string)
 
-                query = db_connect.execute(query_string)
-                for row in query:
-                    post = Post.query.get(row[1])
+                response = db_connect.execute(query_string)
+                for row in response:
+                    post_id = row[0]
+                    post = Post.query.get(post_id)
                     posts.append(post.to_dict())
+
         else:
             posts = [post.to_dict() for post in Post.query.all()]
         payload = {
